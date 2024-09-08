4c9845dac6a00dc0a3a720e74ee4b2044627a601
t-kupp <kupper.thorge@gmail.com>
2024-09-08 14:36:22 +0200

    finished caesarCipher function

--- src/js/caesar-cipher.js
+++ src/js/caesar-cipher.js
@@ -28,25 +28,41 @@ const alphabet = [
 ];
 
 function caesarCipher(string, num) {
-  //throw an error if second argument is not a number
-  if (typeof num !== "number") {
-    return "Invalid arguments. Second argument needs to be a number.";
+  if (!validArguments(string, num)) {
+    return "Invalid arguments.";
   }
-  let letters = string.trim().split("");
+  const letters = string.split("");
   let result = [];
   for (let i = 0; i < letters.length; i++) {
-    let index = alphabet.indexOf(letters[i]);
-    //throw error if string contains characters not found in alphabet
+    let index = alphabet.indexOf(letters[i].toLowerCase());
+    // don't convert non a-z characters
     if (index === -1) {
-      return "Invalid arguments. First argument valid characters: a-z.";
+      result.push(letters[i]);
+      continue;
     }
+    // shift letter by num amount
     for (let j = 0; j < num; j++) {
       index++;
       if (index === 26) index = 0;
     }
-    result.push(alphabet[index]);
+    // handle upper case letters
+    if (letters[i] === letters[i].toUpperCase()) {
+      result.push(alphabet[index].toUpperCase());
+    } else {
+      result.push(alphabet[index]);
+    }
   }
   return result.join("");
+}
+
+function validArguments(string, num) {
+  if (typeof string !== "string") {
+    return false;
+  } else if (typeof num !== "number") {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 export default caesarCipher;
--- src/js/caesar-cipher.test.js
+++ src/js/caesar-cipher.test.js
@@ -1,25 +1,29 @@
 import caesarCipher from "./caesar-cipher";
 
-test('"input: "test", 7 should return "alza""', () => {
+test('input: "test", 7 should return "alza"', () => {
   expect(caesarCipher("test", 7)).toBe("alza");
 });
 
-test('"input: "xyz", 4 should return "bcd""', () => {
+test('input: "xyz", 4 should return "bcd"', () => {
   expect(caesarCipher("xyz", 4)).toBe("bcd");
 });
 
-test('"input: "!5%öäå", 6 should return an error', () => {
-  expect(caesarCipher("!5%öäå", 6)).toBe(
-    "Invalid arguments. First argument valid characters: a-z."
-  );
+test('input: "!5%öäå", 6 should return "!5%öäå"', () => {
+  expect(caesarCipher("!5%öäå", 6)).toBe("!5%öäå");
 });
 
-test('"input: "caesar", "string!@#$" should return an error', () => {
-  expect(caesarCipher("caesar", "string!@#$")).toBe(
-    "Invalid arguments. Second argument needs to be a number."
-  );
+test('input: "JuLiUs.1000!CaEsAr", 8 should return RcTqCa.1000!KiMaIz', () => {
+  expect(caesarCipher("JuLiUs.1000!CaEsAr", 8)).toBe("RcTqCa.1000!KiMaIz");
+});
+
+test('input " 12 white space ! ", 11 should return " 12 hstep dalnp ! "', () => {
+  expect(caesarCipher(" 12 white space ! ", 11)).toBe(" 12 hstep dalnp ! ");
+});
+
+test('input: "caesar", "string!@#$" should return an error', () => {
+  expect(caesarCipher("caesar", "string!@#$")).toBe("Invalid arguments.");
 });
 
 test("empty arguments should return an error", () => {
-  expect(caesarCipher()).toBe("Invalid arguments. Second argument needs to be a number.");
+  expect(caesarCipher()).toBe("Invalid arguments.");
 });
